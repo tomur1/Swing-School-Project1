ec242d5afb73649edd1243c3a6904f88161d891b
tom <tomek.banasiak@gazeta.pl>
2019-04-15 14:06:07 +0200

    Before datastructure creation

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -2,6 +2,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Vector;
 
 public class MainFrame extends JFrame {
     MainFrame() {
@@ -21,7 +22,7 @@ public class MainFrame extends JFrame {
         panelLeft.setLayout(leftGridBagLayout);
 
         JPanel topButtons = new JPanel();
-        JScrollPane centerView = new JScrollPane();
+        JPanel centerView = new JPanel();
         JPanel bottomButtons = new JPanel();
 
         topButtons.setBackground(Color.GREEN);
@@ -86,18 +87,33 @@ public class MainFrame extends JFrame {
         topButtons.add(filter);
 
         //Center view
-        String[] columnNames = new String[5];
-        columnNames[0] = "Path";
-        columnNames[1] = "Autor";
-        columnNames[2] = "Location";
-        columnNames[3] = "Date";
-        columnNames[4] = "Tags";
+        Vector<String> columnNames = new Vector<>();
+        columnNames.add("Path");
+        columnNames.add("Autor");
+        columnNames.add("Location");
+        columnNames.add("Date");
+        columnNames.add("Tags");
 
+        Vector<Vector> data = new Vector<>();
+        Vector<String> firstRow = new Vector<>();
+        firstRow.add("c:/");
+        firstRow.add("Kowalski");
+        firstRow.add("Biesiadowo");
+        firstRow.add("13 piatek");
+        firstRow.add("fajne");
 
-        JTable table = new JTable();
+        data.add(firstRow);
+
+        //MyTableModel tableModel = new MyTableModel(data,columnNames);
+
+        JTable table = new JTable(data, columnNames);
         table.setFillsViewportHeight(true);
+        JScrollPane scrollPane = new JScrollPane(table);
+        table.setPreferredScrollableViewportSize(centerView.getPreferredSize());
+        centerView.setPreferredSize(centerView.getPreferredSize());
+        centerView.add(scrollPane);
 
-        centerView.add(table);
+
 
 
         //Buttons Bottom
@@ -114,15 +130,17 @@ public class MainFrame extends JFrame {
         addPic.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                addPicture();
                 System.out.println("I have been clicked");
             }
         });
-        addPic.setText("Add Picture");
+        addPic.setText("Add");
 
         JButton edit = new JButton();
         edit.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+
                 System.out.println("I have been clicked");
             }
         });
@@ -153,5 +171,113 @@ public class MainFrame extends JFrame {
         pack();
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setVisible(true);
+    }
+
+    //opens a windows to add a picture
+    private void addPicture() {
+        JFrame addWindow = new JFrame("Add Picture");
+        GridBagLayout gridBagLayout = new GridBagLayout();
+
+        addWindow.setLayout(gridBagLayout);
+
+
+        //Initialize fields nad labels
+        JLabel pathLabel = new JLabel("Path: ");
+        JTextField pathField = new JTextField();
+
+        JLabel autorLabel = new JLabel("Autor: ");
+        JTextField autorField = new JTextField();
+
+        JLabel locationLabel = new JLabel("Location: ");
+        JTextField locationField = new JTextField();
+
+        JLabel dateLabel = new JLabel("Data: ");
+        JTextField dateField = new JTextField();
+
+        JLabel tagsLabel = new JLabel("Tags: ");
+        JTextField tagsField = new JTextField();
+
+        GridBagConstraints constraints = new GridBagConstraints();
+        Dimension textSize = new Dimension(200, 20);
+
+        pathField.setPreferredSize(textSize);
+        constraints.gridx = 0;
+        constraints.gridy = 0;
+        constraints.insets = new Insets(8, 8, 2, 8);
+        addWindow.add(pathLabel, constraints);
+        constraints.gridx = 1;
+        constraints.gridy = 0;
+        addWindow.add(pathField, constraints);
+
+        constraints.insets = new Insets(2, 8, 2, 8);
+
+        autorField.setPreferredSize(textSize);
+        constraints.gridx = 0;
+        constraints.gridy = 1;
+        addWindow.add(autorLabel, constraints);
+        constraints.gridx = 1;
+        constraints.gridy = 1;
+        addWindow.add(autorField, constraints);
+
+        locationField.setPreferredSize(textSize);
+        constraints.gridx = 0;
+        constraints.gridy = 2;
+        addWindow.add(locationLabel, constraints);
+        constraints.gridx = 1;
+        constraints.gridy = 2;
+        addWindow.add(locationField, constraints);
+
+        dateField.setPreferredSize(textSize);
+        constraints.gridx = 0;
+        constraints.gridy = 3;
+        addWindow.add(dateLabel, constraints);
+        constraints.gridx = 1;
+        constraints.gridy = 3;
+        addWindow.add(dateField, constraints);
+
+        constraints.insets = new Insets(2, 8, 8, 8);
+
+        tagsField.setPreferredSize(textSize);
+        constraints.gridx = 0;
+        constraints.gridy = 4;
+        addWindow.add(tagsLabel, constraints);
+        constraints.gridx = 1;
+        constraints.gridy = 4;
+        addWindow.add(tagsField, constraints);
+
+
+        //Initialize button
+        JButton accept = new JButton("Accept");
+        accept.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String path = pathField.getText();
+                String autor = autorField.getText();
+                String location = locationField.getText();
+                String date = dateField.getText();
+                String tags = tagsField.getText();
+                System.out.println(path + autor + location + date + tags);
+            }
+        });
+
+        JButton cancel = new JButton("Cancel");
+        cancel.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                addWindow.dispose();
+            }
+        });
+        constraints.gridx = 0;
+        constraints.gridy = 5;
+        addWindow.add(accept, constraints);
+        constraints.gridx = 1;
+        constraints.gridy = 5;
+        constraints.anchor = GridBagConstraints.WEST;
+        addWindow.add(cancel, constraints);
+
+
+        addWindow.pack();
+        addWindow.setVisible(true);
+
     }
 }
--- src/MyTableModel.java
+++ src/MyTableModel.java
@@ -2,18 +2,31 @@ import javax.swing.table.AbstractTableModel;
 
 public class MyTableModel extends AbstractTableModel {
 
+    private String[] columnNames;
+    private Object[][] data;
+
+    MyTableModel() {
+        this.columnNames = new String[]{""};
+        this.data = new String[][]{{""}};
+    }
+
+    MyTableModel(String[] columnNames, String[][] data) {
+        this.columnNames = columnNames;
+        this.data = data;
+    }
+
     @Override
     public int getRowCount() {
-        return 0;
+        return data.length;
     }
 
     @Override
     public int getColumnCount() {
-        return 0;
+        return columnNames.length;
     }
 
     @Override
     public Object getValueAt(int rowIndex, int columnIndex) {
-        return null;
+        return data[rowIndex][columnIndex];
     }
 }
